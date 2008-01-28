dca2c80fd3ab94f2cf3a70c69f3ef1bcd5c51fe6
Serguei Kolos <Serguei.Kolos@cern.ch>
2008-01-28 08:11:45 +0000

    Add possibility to configure ERS streams dynamically, add Throttle and FormattedOutput streams.

---
+++ ers/StandardStreamOutput.h
@@ -0,0 +1,37 @@
+/*
+ *  StandardStreamOutput.h
+ *  ers
+ *
+ *  Created by Serguei Kolos on 02.08.07.
+ *  Copyright 2007 CERN. All rights reserved.
+ *
+ */
+ 
+/** \file StandardStreamOutput.h This file defines C++ standard stream output function for the ERS streams.
+  * \author Serguei Kolos
+  * \brief ers header file 
+  */
+
+#ifndef ERS_STANDARD_STREAM_OUTPUT_H
+#define ERS_STANDARD_STREAM_OUTPUT_H
+
+#include <iostream>
+
+namespace ers
+{
+
+    class Issue;
+    
+    /** This class defines print function for output an issue to any standard C++ output stream.
+      * \author Serguei Kolos
+      * \version 1.0
+      */
+  
+    struct StandardStreamOutput
+    {
+        static std::ostream & print( std::ostream & out, const Issue & issue, int verbosity );
+        static std::ostream & println( std::ostream & out, const Issue & issue, int verbosity );
+    };
+}
+    
+#endif
