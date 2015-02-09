fece274678c5df0aab2f7d6dd00ef23f60f20cef
Serguei Kolos <Serguei.Kolos@cern.ch>
2015-02-09 15:58:54 +0000

    Use spaces instead of tabs.

--- python/ers.py
+++ python/ers.py
@@ -23,30 +23,30 @@ class Severity( object ):
     values = ( DEBUG , LOG, INFO, WARNING, ERROR, FATAL ) = tuple( range( 6 ) )
 
 SeverityNames = tuple ( [ [k for k, v in Severity.__dict__.iteritems() if v == s][0] \
-					for s in Severity.values ] )
+                                        for s in Severity.values ] )
 
 class Context( object ):
     "stores context of an ERS issue"    
     __file = re.sub( r'\.pyc$', '.py', __file__ )
 
     def __init__( self, issue ):
-	self.stack = [f[0] for f in inspect.stack() \
-        		if  f[1] <> self.__file \
+        self.stack = [f[0] for f in inspect.stack() \
+                        if  f[1] <> self.__file \
                         and (      not f[0].f_locals.has_key( 'self' )\
-                        	or not isinstance(f[0].f_locals['self'], Issue))]
+                                or not isinstance(f[0].f_locals['self'], Issue))]
                                 
         self.package_name = issue.__class__.__module__
         class_name    = lambda : self.stack[0].f_locals.has_key( 'self' ) \
-        			 and self.stack[0].f_locals['self'].__class__.__name__ + '.'\
+                                 and self.stack[0].f_locals['self'].__class__.__name__ + '.'\
                                  or ''
 
         self.function_name = class_name() + self.stack[0].f_code.co_name;
-	try:
-	    self.function_name += inspect.formatargvalues( *inspect.getargvalues(self.stack[0]) )
-	except Exception as exx:
-	    self.function_name += '(...)'
-		
-	self.file_name = self.stack[0].f_code.co_filename
+        try:
+            self.function_name += inspect.formatargvalues( *inspect.getargvalues(self.stack[0]) )
+        except Exception as exx:
+            self.function_name += '(...)'
+                
+        self.file_name = self.stack[0].f_code.co_filename
         self.line_number = self.stack[0].f_lineno
         self.host_name = platform.node()
         self.cwd = os.getcwd()
@@ -61,34 +61,34 @@ class Issue( Exception ):
     __verbosity = int( os.getenv( "TDAQ_ERS_VERBOSITY_LEVEL", "0" ) )
     
     def __init__( self, message, kwargs, cause ):
-    	Exception.__init__( self )
+        Exception.__init__( self )
         self.time = time.time()
         self.severity = Severity.ERROR
         self.message = message
         self.cause = cause
         self.__context = Context( self )
         self.qualifiers = [ self.__context.package_name ]
-	self.__dict__.update( kwargs )
+        self.__dict__.update( kwargs )
     
     @property
     def context( self ):
-	return self.__context
+        return self.__context
             
     def isInstanceOf( self, cname ):
-    	return self.__class__.__name__ == cname and True or False
+            return self.__class__.__name__ == cname and True or False
         
     def __repr__( self ):
         pretty_function = lambda f,v: v > 0 \
-        			 and f \
-				 or  re.sub( '\(.*\)', '(...)', f )
+                                 and f \
+                                 or  re.sub( '\(.*\)', '(...)', f )
         s = '%s %s [%s at %s:%d] %s' % ( 
                         SeverityNames[self.severity],
-        		time.strftime( '%Y-%b-%d %H:%M:%S', time.localtime( self.time ) ),
-			pretty_function( self.__context.function_name, self.__verbosity ),
-			self.__context.file_name,
-			self.__context.line_number,
-			self.message )
-	if self.cause <> None:
+                        time.strftime( '%Y-%b-%d %H:%M:%S', time.localtime( self.time ) ),
+                        pretty_function( self.__context.function_name, self.__verbosity ),
+                        self.__context.file_name,
+                        self.__context.line_number,
+                        self.message )
+        if self.cause <> None:
             s += '\n\twas caused by: %s' % repr(self.cause)
         return s
 
@@ -98,7 +98,7 @@ class Issue( Exception ):
 class Message( Issue ):
     "this is wrapper for string message to be sent to debug, log and info streams"
     def __init__( self, msg ):
-    	Issue.__init__( self, msg, {}, None )
+            Issue.__init__( self, msg, {}, None )
 
 if sys.platform == 'linux2':
     import DLFCN
@@ -125,38 +125,38 @@ def info( msg ):
 def warning( issue ):
     "sends issue to the warning stream"
     assert isinstance(issue,Issue), \
-    	'Only an instance of ers.Issue sub-class can be sent to the ers.warning stream'
+            'Only an instance of ers.Issue sub-class can be sent to the ers.warning stream'
     liberspy.warning( issue )
 
 def error( issue ):
     "sends issue to the error stream"
     assert isinstance(issue,Issue), \
-    	'Only an instance of ers.Issue sub-class can be sent to the ers.error stream'
+            'Only an instance of ers.Issue sub-class can be sent to the ers.error stream'
     liberspy.error( issue )
 
 def fatal( issue ):
     "sends issue to the fatal stream"
     assert isinstance(issue,Issue), \
-    	'Only an instance of ers.Issue sub-class can be sent to the ers.fatal stream'
+            'Only an instance of ers.Issue sub-class can be sent to the ers.fatal stream'
     liberspy.fatal( issue )
 
 class LoggingHandler( logging.Handler ) :
-    severity_mapper = { logging.getLevelName( logging.DEBUG )	: lambda m: debug( 0, m ),
-			logging.getLevelName( logging.INFO )	: info,
-			logging.getLevelName( logging.WARNING )	: warning,
-			logging.getLevelName( logging.ERROR )	: error,
-			logging.getLevelName( logging.CRITICAL ): fatal }
+    severity_mapper = { logging.getLevelName( logging.DEBUG )        : lambda m: debug( 0, m ),
+                        logging.getLevelName( logging.INFO )        : info,
+                        logging.getLevelName( logging.WARNING )        : warning,
+                        logging.getLevelName( logging.ERROR )        : error,
+                        logging.getLevelName( logging.CRITICAL ): fatal }
     def __init__(self):
-	logging.Handler.__init__(self)
+        logging.Handler.__init__(self)
         
     def emit(self, record):
         if isinstance( record.msg, Issue ):
             self.severity_mapper[record.levelname]( record.msg )
         else:
             msg = Message( record.msg % record.args )
-	    msg.context.function_name = record.funcName;
-	    msg.context.file_name = record.filename
-	    msg.context.line_number = record.lineno
+            msg.context.function_name = record.funcName;
+            msg.context.file_name = record.filename
+            msg.context.line_number = record.lineno
             msg.context.package_name = record.module
             self.severity_mapper[record.levelname]( msg )
 
@@ -169,5 +169,5 @@ def replaceAllLoggingHandlers( loggername = '' ):
     and adds ERS logging handler to it"""
     l = logging.getLogger( loggername )
     while len( l.handlers ) <> 0:
-	l.removeHandler(l.handlers[0])
+        l.removeHandler(l.handlers[0])
     addLoggingHandler( loggername )
