a208cf40e21d95400a8ae2a9ccfc2e686173b024
Joeal Subash <joeal.subash@cern.ch>
2022-06-09 09:36:14 +0100

    pybind test script added

---
+++ scripts/pybind_test.py
@@ -0,0 +1,18 @@
+import sys
+import ers
+
+print("\nList of everything bound in test binding\n")
+
+print(dir(ers))
+
+print('')
+
+name = sys.argv[1]
+
+lc = ers.LocalContext(name, name,1,name,False)
+
+ai = ers.AnyIssue(name, lc, name)
+
+nm = ai.get_class_name()
+
+print(nm)
