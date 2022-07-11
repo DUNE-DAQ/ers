ed1aa8a651c58e1c242ca40877e38d715e7a0129
Joeal Subash <joeal.subash@cern.ch>
2022-07-12 00:58:09 +0100

    minimum working example in scripts/pybind11_test.py

---
+++ scripts/pybind11_test.py
@@ -0,0 +1,70 @@
+import sys
+import ers
+import logging
+"""
+#print("\nList of everything bound in test binding\n")
+
+#print(dir(ers))
+
+#print('')
+
+name = sys.argv[1]
+
+lc = ers.LocalContext(name, name,1,name,False)
+
+ai = ers.AnyIssue(name, lc, name)
+
+#nm = ai.get_class_name()
+
+#print(nm)
+
+
+
+ers.error(ai)
+
+"""
+
+
+class PermissionDenied( ers.PyIssue ):
+    def __init__( self, cause = "" ):
+        ers.PyIssue.__init__( self, 'Permission denied', {}, cause )
+
+class CantOpenFile( ers.PyIssue ):
+    def __init__( self, fname, err, cause = "" ):
+        ers.PyIssue.__init__( self, 'Can not open "%s" file, error = %d' % ( fname, err ),
+	    { 'filename' : fname, 'error' : err },
+	    cause )
+
+class Test(object):
+    def method(self):
+        raise PermissionDenied( )
+
+def test_function( arg1, arg2 ):
+    format = 'This is a %s message #%d'
+    name = "JOEL"
+    lc = ers.LocalContext(name, name,1,name,False)
+    ers.info(ers.AnyIssue("HEY", lc, "YOU"))
+    #print("Using python logging\n")
+    #logging.info(format, 'info', 2 )
+    #logging.warning(format, 'warning', 3 )
+    #logging.error(format, 'error', 4 )
+    #logging.critical(format, 'critical', 5 )
+
+    try:
+        t = Test( )
+        t.method( )
+    #except:
+    #    pass
+    except PermissionDenied as e:
+        raise CantOpenFile( "test.py", 13, e )
+
+import time
+
+if __name__ == "__main__":
+
+    try:
+    	test_function( "test", "ers" )
+    except ers.PyIssue as e:
+        ers.warning(e.ai)
+        print(type(e.cause))
+        print(type(e.message))
