9884f1bdd29c65181384dbf3265898eb9264367a
Marco Roda <mroda@liverpool.ac.uk>
2023-03-16 12:56:20 +0100

    Update test app

--- test/apps/ers_inheritance.cxx
+++ test/apps/ers_inheritance.cxx
@@ -2,27 +2,50 @@
 
 #include <ers/ers.hpp>
 
-ERS_DECLARE_ISSUE( test, ExceptionTest, "Exception test", ERS_EMPTY )
+ERS_DECLARE_ISSUE( test, Base, "Exception test", ERS_EMPTY )
 
 ERS_DECLARE_ISSUE_BASE(test,                                         // namespace name
 		       daughter,                                     // issue name
-                       test::ExceptionTest,                          // base issue name
+                       test::Base,                                   // base issue name
 		       "I'm inheriting ",                            // message
+		       ERS_EMPTY,                                    // base class attributes
+                       ERS_EMPTY                                     // this class attributes
+)
+
+ERS_DECLARE_ISSUE_BASE(other,                                        // namespace name
+		       A,                                            // issue name
+                       test::daughter,                               // base issue name
+		       "I'm inheriting from daughter",               // message
+		       ERS_EMPTY,                                    // base class attributes
+                       ERS_EMPTY                                     // this class attributes
+)
+
+ERS_DECLARE_ISSUE_BASE(weird,                                        // namespace name
+		       B,                                            // issue name
+                       other::A,                                     // base issue name
+		       "I'm inheriting from A",                      // message
 		       ERS_EMPTY,                                    // base class attributes
                        ERS_EMPTY                                     // this class attributes
 )
 
 
 
+
 int main(int ac, char** av) {
 
-  test::ExceptionTest a( ERS_HERE );
-
-  test::daughter b( ERS_HERE );
-
-  std::cout << a.inheritance_chain() << std::endl;
-  std::cout << b.inheritance_chain() << std::endl;
-
-    
+  std::vector<ers::Issue> issues;
+  issues.push_back( test::Base(ERS_HERE) );
+  issues.push_back( test::daughter(ERS_HERE) );
+  issues.push_back( other::A(ERS_HERE) );
+  issues.push_back( weird::B(ERS_HERE) );
+  
+  for ( const auto & i : issues ) {
+    auto chain = i.inheritance_chain();
+    for ( const auto & c : chain ) {
+      std::cout << c << "\t" ;
+    }
+    std::cout << std::endl;
+  }
+  
   return 0;
 }
